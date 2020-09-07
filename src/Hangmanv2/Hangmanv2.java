0cab2320d14ce5470b2c0edf99750731e09d4da2
JoacimStahl96 <69908677+JoacimStahl96@users.noreply.github.com>
2020-09-07 10:04:40 +0200

    updated with a menu

--- src/Hangmanv2/Hangmanv2.java
+++ src/Hangmanv2/Hangmanv2.java
@@ -3,45 +3,57 @@ package Hangmanv2;
 import java.util.Scanner;
 
 public class Hangmanv2 {
-	 
-	static String word = Wordlist.getWord();
-    private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
-	public static int counter = 0;  
-    
-    
+
+	static String word = Wordlist.getWord(); // connects this class to a getter in the Wordlist class.
+	private static String asterisk = new String(new char[word.length()]).replace("\0", "*"); // Grabs and hides the
+																								// secret word in
+																								// asterisks
+	public static int counter = 0; // counter for the game
+	static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-		System.out.println("You can write either a single letter or a complete word. ");
-	    System.out.println("However, you lose instantly if you write a word that is wrong. ");
-		while (counter < 7 && asterisk.contains("*")) {
-			
-			
-			System.out.println("You have 7 chances to succeed, number of fails "+counter);
-			System.out.println("Guess any letter or word ");
-			
-			System.out.println(asterisk);
+
+		System.out.println("You can write either a single letter or a complete word."
+				+ "\nYou have a total of 7 chances with letters.");
+		System.out.println("However, you lose instantly if you write a word that is wrong.");
+		System.out.println("Please pick an option" + "\n'1' to write a whole word." + "\n'2' to write a letter."
+				+ "\n'3' to see how many errors you've made.");
+
+		boolean stayalive = false; // boolean to help terminate the loop with entireWords()
+		while (counter < 7 && asterisk.contains("*") && stayalive == false) { // game loop
+
+			System.out.println(asterisk); // prints the hidden words in asterisk
+
 			String Guess = input.next().toUpperCase();
-			
-			
-			
-	        if(Guess.length() > 1) {
-			entireWords();
-			break;
-			}else {
-				hang(Guess);
-			} 
-			
-			
+
+			switch (Guess) { // the menu to choose between a word, char and the counter.
+			  case "1": // if user enters this case, the game will end after a guess has been made
+				entireWords();
+				stayalive = true;
+				break;
+			  case "2":
+				letters();
+				break;
+			  case "3":
+				System.out.println("You've guessed wrong " + counter + " times");
+				break;
+
+			  default:
+				System.out.println("Please enter an actual choice.");
+			}
+
 		}
 		input.close();
+
 	}
 
-	public static void hang(String guess) { // method for Char guessing
+	public static void letters() { // method for Char guessing ( single letter guessing )
+		String guess = input.next().toUpperCase();
 		String newasterisk = "";
 		for (int i = 0; i < word.length(); i++) {
-			if (word.charAt(i) == guess.charAt(0)) {
+			if (word.charAt(i) == guess.charAt(0)) { // correct guess
 				newasterisk += guess.charAt(0);
-			} else if (asterisk.charAt(i) != '*') {
+			} else if (asterisk.charAt(i) != '*') { // wrong guess
 				newasterisk += word.charAt(i);
 			} else {
 				newasterisk += "*";
@@ -52,19 +64,20 @@ public class Hangmanv2 {
 			counter++;
 			imageGetsHanged();
 		} else {
-			asterisk = newasterisk;
+			asterisk = newasterisk; // a single correct letter entered.
 		}
-		if (asterisk.equals(word)) {
+		if (asterisk.equals(word)) { // correct letters were entered and wins the game
 			System.out.println("Correct! The word was " + word);
 		}
 	}
-	
+
 	public static void entireWords() { // the entire word method, guess right = win, guess wrong = lose.
-        
-		if(!asterisk.equals(word)) {
-			System.out.println(" You win! The word was " + word);
-			
-		}else {
+		String guessWord = input.next().toUpperCase();
+		if (guessWord.equals(word)) {
+			System.out.println("You win! The word was " + word);
+
+		} else if (!guessWord.equals(word)) {
+
 			System.out.println("GAME OVER!");
 			System.out.println("   ____________");
 			System.out.println("   |          _|_");
@@ -76,12 +89,11 @@ public class Hangmanv2 {
 			System.out.println("   |          / \\ ");
 			System.out.println("___|___      /   \\");
 			System.out.println("YOU'RE HANGED! The word was " + word);
-			
-		}
-	
-	} 
 
-	public static void imageGetsHanged() { // shows the error progress 
+		}
+	}
+
+	public static void imageGetsHanged() { // shows the wrong guesses progress with images connected to the counter
 		if (counter == 1) {
 			System.out.println("Wrong guess, try again");
 			System.out.println();
@@ -164,5 +176,5 @@ public class Hangmanv2 {
 			System.out.println("YOU'RE HANGED! The word was " + word);
 		}
 	}
-	
+
 }
--- src/Hangmanv2/Wordlist.java
+++ src/Hangmanv2/Wordlist.java
@@ -1,22 +1,14 @@
 package Hangmanv2;
 
 public class Wordlist {
-	
-	private static String[] words = {"MUPP", "PEN", "PLAYGROUND", "BOTTLE", "MOUSE", "VARIABLE",
-			"SCREEN", "CLOCK", "CRISPS", "EGG", "CHICKEN", "CHAIR",
-			 "SUPERCALIFRAGILISTICEXPIALIDOCIUS" };
-	
-	
-	private static String word = words[(int) (Math.random() * words.length)];
+	// words -> simply the library from which to pick a secret word
+	private static String[] words = { "MUPP", "PEN", "PLAYGROUND", "BOTTLE", "MOUSE", "VARIABLE", "SCREEN", "CLOCK",
+			"CRISPS", "EGG", "CHICKEN", "CHAIR", "SUPERCALIFRAGILISTICEXPIALIDOCIUS" };
 
+	private static String word = words[(int) (Math.random() * words.length)]; // picks the secret word.
 
-	public static String getWord() {
+	public static String getWord() { // makes it possible to grab String word in class hangmanv2.
 		return word;
-	}
-
-
-	public static void setWord(String word) {
-		Wordlist.word = word;
 	}
 
 }
